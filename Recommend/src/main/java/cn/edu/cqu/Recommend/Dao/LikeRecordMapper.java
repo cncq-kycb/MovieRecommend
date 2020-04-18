2afc1e899797092675b4bcf6579e6d24d51be4d5
AlbertoWang <albertowang@foxmail.com>
2020-04-19 02:21:23 +0800

    行为日志DAO

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/LikeRecordMapper.java
@@ -0,0 +1,30 @@
+package cn.edu.cqu.Recommend.Dao;
+
+import cn.edu.cqu.Recommend.Pojo.LikeRecord;
+import cn.edu.cqu.Recommend.Pojo.LikeRecordExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface LikeRecordMapper {
+    long countByExample(LikeRecordExample example);
+
+    int deleteByExample(LikeRecordExample example);
+
+    int deleteByPrimaryKey(Integer likeRecordId);
+
+    int insert(LikeRecord record);
+
+    int insertSelective(LikeRecord record);
+
+    List<LikeRecord> selectByExample(LikeRecordExample example);
+
+    LikeRecord selectByPrimaryKey(Integer likeRecordId);
+
+    int updateByExampleSelective(@Param("record") LikeRecord record, @Param("example") LikeRecordExample example);
+
+    int updateByExample(@Param("record") LikeRecord record, @Param("example") LikeRecordExample example);
+
+    int updateByPrimaryKeySelective(LikeRecord record);
+
+    int updateByPrimaryKey(LikeRecord record);
+}
---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/ViewRecordMapper.java
@@ -0,0 +1,30 @@
+package cn.edu.cqu.Recommend.Dao;
+
+import cn.edu.cqu.Recommend.Pojo.ViewRecord;
+import cn.edu.cqu.Recommend.Pojo.ViewRecordExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ViewRecordMapper {
+    long countByExample(ViewRecordExample example);
+
+    int deleteByExample(ViewRecordExample example);
+
+    int deleteByPrimaryKey(Integer viewRecordId);
+
+    int insert(ViewRecord record);
+
+    int insertSelective(ViewRecord record);
+
+    List<ViewRecord> selectByExample(ViewRecordExample example);
+
+    ViewRecord selectByPrimaryKey(Integer viewRecordId);
+
+    int updateByExampleSelective(@Param("record") ViewRecord record, @Param("example") ViewRecordExample example);
+
+    int updateByExample(@Param("record") ViewRecord record, @Param("example") ViewRecordExample example);
+
+    int updateByPrimaryKeySelective(ViewRecord record);
+
+    int updateByPrimaryKey(ViewRecord record);
+}
