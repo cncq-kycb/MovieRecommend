d21dbd3545d09b940cb24fa7405c639c9e081bf0
AlbertoWang <albertowang@foxmail.com>
2020-04-19 12:57:53 +0800

    静态字符串文件

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Utils/Static/LogioStrings.java
@@ -0,0 +1,12 @@
+package cn.edu.cqu.Recommend.Utils.Static;
+
+public class LogioStrings {
+
+	// Login 相关
+	public static final String NO_ACCOUNT = "账号不存在";
+	public static final String WRONG_PASSWORD = "密码错误";
+	public static final String LOGIN_SUCCESS = "登录成功";
+
+	// Logout 相关
+	public static final String LOGOUT_SUCCESS = "登出成功";
+}
