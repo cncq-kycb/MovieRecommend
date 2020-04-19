5d7a9d2a5981ac10d45f260d00891fa7b88ab326
AlbertoWang <albertowang@foxmail.com>
2020-04-19 19:39:06 +0800

    添加获取用户信息请求响应

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
@@ -1,14 +1,26 @@
 package cn.edu.cqu.Recommend.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.edu.cqu.Recommend.Service.UserService;
+import cn.edu.cqu.Recommend.Utils.MyJson;
 
 @RestController
 public class UserController {
 
 	@Autowired
 	UserService userService;
+
+	// 获取用户信息
+	@RequestMapping
+	@ResponseBody
+	public MyJson getUserInfo(HttpSession session) {
+		return userService.getUserInfo(session);
+	}
 
 }
