1673ab7d5bb2fc9cf376d41592d8aa4279cc2f76
AlbertoWang <albertowang@foxmail.com>
2020-04-19 12:59:24 +0800

    新增用户登陆、登出接口

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ActionService.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ActionService.java
@@ -1,8 +1,19 @@
 package cn.edu.cqu.Recommend.Service;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Service;
+
+import cn.edu.cqu.Recommend.Pojo.User;
+import cn.edu.cqu.Recommend.Utils.MyJson;
 
 @Service
 public interface ActionService {
+
+	// 登陆服务
+	MyJson login(User user, HttpSession session);
+
+	// 登出服务
+	MyJson logout(HttpSession session);
 
 }
