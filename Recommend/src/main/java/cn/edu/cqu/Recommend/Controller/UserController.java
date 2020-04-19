861ef9a46e831559c2a22f0958eaf311cd2f2e3f
AlbertoWang <albertowang@foxmail.com>
2020-04-19 11:18:19 +0800

    用户请求控制器

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
@@ -0,0 +1,13 @@
+package cn.edu.cqu.Recommend.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import cn.edu.cqu.Recommend.Service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	UserService userService;
+}
