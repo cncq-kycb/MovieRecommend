280c0adf7d19e4184d30b74574d1ae94208bd1f7
AlbertoWang <albertowang@foxmail.com>
2020-04-19 14:38:12 +0800

    添加URI响应

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.edu.cqu.Recommend.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.edu.cqu.Recommend.Service.UserService;
@@ -10,4 +11,9 @@ public class UserController {
 
 	@Autowired
 	UserService userService;
+
+	@RequestMapping("/index")
+	public String gotoIndex() {
+		return "index";
+	}
 }
