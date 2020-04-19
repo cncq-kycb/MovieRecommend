894907ec9927079390b7893bed111e36c298f665
AlbertoWang <albertowang@foxmail.com>
2020-04-19 11:16:11 +0800

    用户相关DAO

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/UserMapper.java
@@ -0,0 +1,34 @@
+package cn.edu.cqu.Recommend.Dao;
+
+import cn.edu.cqu.Recommend.Pojo.User;
+import cn.edu.cqu.Recommend.Pojo.UserExample;
+import cn.edu.cqu.Recommend.Pojo.UserKey;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int deleteByPrimaryKey(UserKey key);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    User selectByPrimaryKey(UserKey key);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
