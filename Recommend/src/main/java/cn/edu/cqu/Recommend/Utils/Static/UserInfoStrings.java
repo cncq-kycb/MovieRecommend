814b10d6dff6b80c4271b036e5487b34b280539e
AlbertoWang <albertowang@foxmail.com>
2020-04-19 20:03:03 +0800

    用户信息相关提示

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Utils/Static/UserInfoStrings.java
@@ -0,0 +1,10 @@
+package cn.edu.cqu.Recommend.Utils.Static;
+
+public class UserInfoStrings {
+
+	// 注册部分
+	public static final String TEL_EXIST = "该手机号已被注册";
+	public static final String SIGNUP_SUCCESS = "注册成功";
+	public static final String SIGNUP_FAILED = "注册失败";
+
+}
