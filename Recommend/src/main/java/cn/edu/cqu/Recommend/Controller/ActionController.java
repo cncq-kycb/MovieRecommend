ea2b0a485446e25dfa4cb82e00a77e27665ff48c
AlbertoWang <albertowang@foxmail.com>
2020-04-19 14:27:13 +0800

    响应登录、登出请求

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/ActionController.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/ActionController.java
@@ -1,9 +1,15 @@
 package cn.edu.cqu.Recommend.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import cn.edu.cqu.Recommend.Pojo.User;
 import cn.edu.cqu.Recommend.Service.ActionService;
+import cn.edu.cqu.Recommend.Utils.MyJson;
 
 @RestController
 public class ActionController {
@@ -11,4 +17,17 @@ public class ActionController {
 	@Autowired
 	ActionService actionService;
 
+	// 登录请求
+	@GetMapping(value = "/login")
+	@ResponseBody
+	public MyJson login(User user, HttpSession session) {
+		return actionService.login(user, session);
+	}
+
+	// 登出请求
+	@GetMapping(value = "/logout")
+	@ResponseBody
+	public MyJson logout(User user, HttpSession session) {
+		return actionService.logout(session);
+	}
 }
