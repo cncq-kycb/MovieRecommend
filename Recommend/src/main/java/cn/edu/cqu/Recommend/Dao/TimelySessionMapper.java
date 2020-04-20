24fccda11f24f5830f842493269e97a01d2a320b
AlbertoWang <albertowang@foxmail.com>
2020-04-20 14:39:20 +0800

    新增时效性电影场次POJO与DAO

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/TimelySessionMapper.java
@@ -0,0 +1,22 @@
+package cn.edu.cqu.Recommend.Dao;
+
+import cn.edu.cqu.Recommend.Pojo.TimelySession;
+import cn.edu.cqu.Recommend.Pojo.TimelySessionExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TimelySessionMapper {
+    long countByExample(TimelySessionExample example);
+
+    int deleteByExample(TimelySessionExample example);
+
+    int insert(TimelySession record);
+
+    int insertSelective(TimelySession record);
+
+    List<TimelySession> selectByExample(TimelySessionExample example);
+
+    int updateByExampleSelective(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
+
+    int updateByExample(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
+}
