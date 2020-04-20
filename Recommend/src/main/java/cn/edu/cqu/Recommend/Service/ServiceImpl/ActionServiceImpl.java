f1b8ebea6956e71583fc884083e6840aa233d2e9
AlbertoWang <albertowang@foxmail.com>
2020-04-20 19:28:10 +0800

    删除多余包

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/ActionServiceImpl.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/ActionServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.annotation.MergedAnnotations.SearchStrategy;
 import org.springframework.stereotype.Component;
 
 import cn.edu.cqu.Recommend.Dao.SearchRecordMapper;
