84a3d99eed899b873c165aa9591e935cc6d486b5
AlbertoWang <albertowang@foxmail.com>
2020-04-20 15:34:14 +0800

    增加、实现按日期条件获取电影场次接口

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/SessionService.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/SessionService.java
@@ -2,7 +2,12 @@ package cn.edu.cqu.Recommend.Service;
 
 import org.springframework.stereotype.Service;
 
+import cn.edu.cqu.Recommend.Utils.MyJson;
+
 @Service
 public interface SessionService {
+
+	// 按日期条件获取电影场次信息
+	public MyJson getTimelySession(String condition);
 
 }
