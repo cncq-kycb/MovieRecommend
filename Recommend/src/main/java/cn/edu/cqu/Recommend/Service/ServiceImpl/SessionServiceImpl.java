6f6e212c50c49fbfead46a0a3a3f1c3840ffc550
AlbertoWang <albertowang@foxmail.com>
2020-04-20 13:21:16 +0800

    电影场次相关服务接口初始化

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/SessionServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.edu.cqu.Recommend.Service.ServiceImpl;
+
+import org.springframework.stereotype.Component;
+
+import cn.edu.cqu.Recommend.Service.SessionService;
+
+@Component
+public class SessionServiceImpl implements SessionService {
+
+}
---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/SessionService.java
@@ -0,0 +1,8 @@
+package cn.edu.cqu.Recommend.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface SessionService {
+
+}
