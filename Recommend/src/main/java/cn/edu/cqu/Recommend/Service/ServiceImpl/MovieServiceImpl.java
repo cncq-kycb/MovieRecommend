9fc936a9a04c1e40c55bdc31df2d901dd7cc45b8
AlbertoWang <albertowang@foxmail.com>
2020-04-20 13:20:45 +0800

    电影相关服务接口初始化

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/MovieServiceImpl.java
@@ -0,0 +1,10 @@
+package cn.edu.cqu.Recommend.Service.ServiceImpl;
+
+import org.springframework.stereotype.Component;
+
+import cn.edu.cqu.Recommend.Service.MovieService;
+
+@Component
+public class MovieServiceImpl implements MovieService {
+
+}
