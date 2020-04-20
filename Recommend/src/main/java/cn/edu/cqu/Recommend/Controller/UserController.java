acf9179473c1a866af99b57e67348bd02949d554
AlbertoWang <albertowang@foxmail.com>
2020-04-20 16:31:03 +0800

    增加了获取今、明天电影场次的响应

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/UserController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,20 @@ public class UserController {
 	@ResponseBody
 	public MyJson getUserInfo(HttpSession session) {
 		return userService.getUserInfo(session);
+	}
+
+	// 获取今日电影场次信息
+	@GetMapping(value = "/getSessionInfoToday")
+	@ResponseBody
+	public MyJson getSessionInfoToday(@RequestParam(required = true, value = "movieId") Integer movieId) {
+		return userService.getTimelySession(movieId, "TODAY");
+	}
+
+	// 获取明日电影场次信息
+	@GetMapping(value = "/getSessionInfoTomorrow")
+	@ResponseBody
+	public MyJson getSessionInfoTomorrow(@RequestParam(required = true, value = "movieId") Integer movieId) {
+		return userService.getTimelySession(movieId, "TOMORROW");
 	}
 
 }
