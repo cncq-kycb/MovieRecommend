f274b02bdf02ef7c4c57af7a47b8ce8713bfd0c6
AlbertoWang <albertowang@foxmail.com>
2020-04-20 15:41:04 +0800

    添加Mapper注解

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/TimelySessionMapper.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Dao/TimelySessionMapper.java
@@ -3,20 +3,23 @@ package cn.edu.cqu.Recommend.Dao;
 import cn.edu.cqu.Recommend.Pojo.TimelySession;
 import cn.edu.cqu.Recommend.Pojo.TimelySessionExample;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface TimelySessionMapper {
-    long countByExample(TimelySessionExample example);
+	long countByExample(TimelySessionExample example);
 
-    int deleteByExample(TimelySessionExample example);
+	int deleteByExample(TimelySessionExample example);
 
-    int insert(TimelySession record);
+	int insert(TimelySession record);
 
-    int insertSelective(TimelySession record);
+	int insertSelective(TimelySession record);
 
-    List<TimelySession> selectByExample(TimelySessionExample example);
+	List<TimelySession> selectByExample(TimelySessionExample example);
 
-    int updateByExampleSelective(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
+	int updateByExampleSelective(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
 
-    int updateByExample(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
+	int updateByExample(@Param("record") TimelySession record, @Param("example") TimelySessionExample example);
 }
