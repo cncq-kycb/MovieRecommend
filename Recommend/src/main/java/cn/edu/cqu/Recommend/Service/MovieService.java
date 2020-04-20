545db015c2595b202c61ef4fb93ee38d6df3f59c
AlbertoWang <albertowang@foxmail.com>
2020-04-20 14:57:08 +0800

    新增获取全部电影信息、按条件搜索电影信息接口

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/MovieService.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/MovieService.java
@@ -2,7 +2,15 @@ package cn.edu.cqu.Recommend.Service;
 
 import org.springframework.stereotype.Service;
 
+import cn.edu.cqu.Recommend.Utils.MyJson;
+
 @Service
 public interface MovieService {
+
+	// 获取全部电影信息
+	public MyJson getMovies();
+
+	// 按条件搜索电影信息
+	public MyJson searchMovie(String condition);
 
 }
