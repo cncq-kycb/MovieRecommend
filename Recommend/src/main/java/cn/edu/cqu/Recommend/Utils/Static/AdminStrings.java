8a2b4b4820bd280daf0f3a373863d844634bc156
AlbertoWang <albertowang@foxmail.com>
2020-04-21 22:16:09 +0800

    管理员接口提示

---
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Utils/Static/AdminStrings.java
@@ -0,0 +1,13 @@
+package cn.edu.cqu.Recommend.Utils.Static;
+
+public class AdminStrings {
+
+	public static final String TIMER_ON = "定时任务(爬虫)开启";
+
+	public static final String TIMER_OFF = "定时任务(爬虫)关闭";
+
+	public static final String TIMER_ON_ERR = "定时任务(爬虫)开启失败";
+
+	public static final String TIMER_OFF_ERR = "定时任务(爬虫)关闭失败";
+
+}
