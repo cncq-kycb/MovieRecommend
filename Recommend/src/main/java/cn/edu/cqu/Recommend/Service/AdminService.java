ec46c55b31cee9ff79bf9410af181c872917e8b4
AlbertoWang <albertowang@foxmail.com>
2020-04-26 14:01:37 +0800

    实现爬虫和离线推荐的定时任务

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/AdminService.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/AdminService.java
@@ -5,10 +5,13 @@ import org.springframework.stereotype.Service;
 @Service
 public interface AdminService {
 
+	// type=1: 爬虫开关
+	// type=2: 推荐开关
+
 	// 开启定时任务
-	public String scheduleSwitchOn();
+	public String scheduleSwitchOn(int type);
 
 	// 关闭定时任务
-	public String scheduleSwitchOff();
+	public String scheduleSwitchOff(int type);
 
 }
--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/AdminServiceImpl.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/AdminServiceImpl.java
@@ -11,33 +11,83 @@ import cn.edu.cqu.Recommend.Utils.Static.AdminStrings;
 public class AdminServiceImpl implements AdminService {
 
 	// 定时任务开关
-	private boolean timerSwitch = false;
+	private boolean timerSwitchSpider = false;
+	private boolean timerSwitchRecommend = false;
+	// 推荐个数
+	private int recommendSize = 10;
 
 	@Override
-	public String scheduleSwitchOn() {
-		if (timerSwitch == false) {
-			timerSwitch = true;
-			return AdminStrings.TIMER_ON;
+	public String scheduleSwitchOn(int type) {
+		switch (type) {
+		case 1:
+			if (this.timerSwitchSpider == false) {
+				this.timerSwitchSpider = true;
+				return AdminStrings.TIMER_ON_SPIDER;
+			}
+			break;
+		case 2:
+			if (this.timerSwitchRecommend == false) {
+				this.timerSwitchRecommend = true;
+				return AdminStrings.TIMER_ON_RECOMMEND;
+			}
+			break;
+		default:
+			break;
 		}
 		return AdminStrings.TIMER_ON_ERR;
 	}
 
 	@Override
-	public String scheduleSwitchOff() {
-		if (timerSwitch == true) {
-			timerSwitch = false;
-			return AdminStrings.TIMER_OFF;
+	public String scheduleSwitchOff(int type) {
+		switch (type) {
+		case 1:
+			if (this.timerSwitchSpider == true) {
+				this.timerSwitchSpider = false;
+				return AdminStrings.TIMER_OFF_SPIDER;
+			}
+			break;
+		case 2:
+			if (this.timerSwitchRecommend == true) {
+				this.timerSwitchRecommend = false;
+				return AdminStrings.TIMER_OFF_RECOMMEND;
+			}
+			break;
+		default:
+			break;
 		}
 		return AdminStrings.TIMER_OFF_ERR;
 	}
 
+	// 爬虫任务
 	// 每天00:00执行
-	@Scheduled(cron = "0 0 0 * * ?")
-	private void scheduledTask(int recommendSize) throws InterruptedException {
-		if (timerSwitch == true) {
+	// @Scheduled(cron = "0 0 0 * * ?")
+	// 演示用每隔1000 * n秒执行一次任务
+	@Scheduled(fixedDelay = 1000 * 10)
+	private void scheduledSpiderTask() throws InterruptedException {
+		if (this.timerSwitchRecommend == true) {
 			// TODO: 插入定时任务
 			String[] argv = new String[] { "/Users/alberto/anaconda3/bin/python",
-					"./src/main/resources/Script/recommend/RecommendSys.py", recommendSize+"" };
+					"./src/main/resources/Script/spider/.py", this.recommendSize + "" };
+			String result;
+			try {
+				result = Utils.cmdCall(argv);
+				System.out.println(result);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 推荐任务
+	// 每天00:00执行
+	// @Scheduled(cron = "0 0 0 * * ?")
+	// 演示用每隔1000 * n秒执行一次任务
+	@Scheduled(fixedDelay = 1000 * 10)
+	private void scheduledRecommendTask() throws InterruptedException {
+		if (this.timerSwitchRecommend == true) {
+			// TODO: 插入定时任务
+			String[] argv = new String[] { "/Users/alberto/anaconda3/bin/python",
+					"./src/main/resources/Script/recommend/RecommendSys.py", this.recommendSize + "" };
 			String result;
 			try {
 				result = Utils.cmdCall(argv);
