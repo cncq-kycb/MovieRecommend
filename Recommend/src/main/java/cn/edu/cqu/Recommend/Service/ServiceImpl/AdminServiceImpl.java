782d7219d775f01a014ad373c4b024d2bdf99432
AlbertoWang <albertowang@foxmail.com>
2020-04-26 13:28:46 +0800

    添加定时任务

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/AdminServiceImpl.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Service/ServiceImpl/AdminServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import cn.edu.cqu.Recommend.Service.AdminService;
+import cn.edu.cqu.Recommend.Utils.Utils;
 import cn.edu.cqu.Recommend.Utils.Static.AdminStrings;
 
 @Component
@@ -32,9 +33,18 @@ public class AdminServiceImpl implements AdminService {
 
 	// 每天00:00执行
 	@Scheduled(cron = "0 0 0 * * ?")
-	private void scheduledTask() {
+	private void scheduledTask(int recommendSize) throws InterruptedException {
 		if (timerSwitch == true) {
 			// TODO: 插入定时任务
+			String[] argv = new String[] { "/Users/alberto/anaconda3/bin/python",
+					"./src/main/resources/Script/recommend/RecommendSys.py", recommendSize+"" };
+			String result;
+			try {
+				result = Utils.cmdCall(argv);
+				System.out.println(result);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
