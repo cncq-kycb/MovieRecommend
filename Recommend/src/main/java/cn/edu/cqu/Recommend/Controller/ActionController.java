f3e40e23f464c39f53cfa7e85b5c3e46dc363050
AlbertoWang <albertowang@foxmail.com>
2020-04-26 14:01:47 +0800

    实现定时任务响应

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/ActionController.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Controller/ActionController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.edu.cqu.Recommend.Pojo.User;
 import cn.edu.cqu.Recommend.Service.ActionService;
+import cn.edu.cqu.Recommend.Service.AdminService;
 import cn.edu.cqu.Recommend.Utils.MyJson;
 
 @RestController
@@ -17,6 +19,8 @@ public class ActionController {
 
 	@Autowired
 	ActionService actionService;
+	@Autowired
+	AdminService adminService;
 
 	// 登录请求
 	@PostMapping(value = "/login")
@@ -37,5 +41,33 @@ public class ActionController {
 	@ResponseBody
 	public MyJson signup(User user) {
 		return actionService.signup(user);
+	}
+
+	// 开启爬虫任务
+	@RequestMapping(value = "/spiderScheduleSwitchOn")
+	@ResponseBody
+	public String spiderScheduleSwitchOn() {
+		return adminService.scheduleSwitchOn(1);
+	}
+
+	// 关闭爬虫任务
+	@RequestMapping(value = "/spiderScheduleSwitchOff")
+	@ResponseBody
+	public String spiderScheduleSwitchOff() {
+		return adminService.scheduleSwitchOff(1);
+	}
+
+	// 开启推荐任务
+	@RequestMapping(value = "/recommendScheduleSwitchOn")
+	@ResponseBody
+	public String recommendScheduleSwitchOn() {
+		return adminService.scheduleSwitchOn(2);
+	}
+
+	// 关闭推荐任务
+	@RequestMapping(value = "/recommendScheduleSwitchOff")
+	@ResponseBody
+	public String recommendScheduleSwitchOff() {
+		return adminService.scheduleSwitchOff(2);
 	}
 }
