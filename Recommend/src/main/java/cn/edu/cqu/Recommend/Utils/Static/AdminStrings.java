ddf74800e8cad6fd9c4b46abce348a387e3968b4
AlbertoWang <albertowang@foxmail.com>
2020-04-26 14:01:19 +0800

    新增提示符

--- Recommend/src/main/java/cn/edu/cqu/Recommend/Utils/Static/AdminStrings.java
+++ Recommend/src/main/java/cn/edu/cqu/Recommend/Utils/Static/AdminStrings.java
@@ -2,12 +2,16 @@ package cn.edu.cqu.Recommend.Utils.Static;
 
 public class AdminStrings {
 
-	public static final String TIMER_ON = "定时任务(爬虫)开启";
+	public static final String TIMER_ON_SPIDER = "定时任务(爬虫)开启";
 
-	public static final String TIMER_OFF = "定时任务(爬虫)关闭";
+	public static final String TIMER_OFF_SPIDER = "定时任务(爬虫)关闭";
 
-	public static final String TIMER_ON_ERR = "定时任务(爬虫)开启失败";
+	public static final String TIMER_ON_RECOMMEND = "定时任务(离线推荐)开启";
 
-	public static final String TIMER_OFF_ERR = "定时任务(爬虫)关闭失败";
+	public static final String TIMER_OFF_RECOMMEND = "定时任务(离线推荐)关闭";
+
+	public static final String TIMER_ON_ERR = "定时任务开启失败";
+
+	public static final String TIMER_OFF_ERR = "定时任务关闭失败";
 
 }
